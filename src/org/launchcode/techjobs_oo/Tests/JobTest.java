1765436c95f06de5fdbd2adbdeea00f49fc60e7d
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-10 18:05:34 -0500

    got set up to start writing unit tests

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,17 @@
+package org.launchcode.techjobs_oo.Tests;
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.Job;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
+public class JobTest {
+    @Before
+    public void createJobObjects() {
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+    }
+
+}
