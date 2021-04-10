446adcc8aa38687f1862aecb1ed5286d54fc1544
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-10 18:16:20 -0500

    JobTests passes first test

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,17 +1,23 @@
 package org.launchcode.techjobs_oo.Tests;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.Job;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 
 public class JobTest {
+    Job jobOne;
+    Job jobTwo;
     @Before
     public void createJobObjects() {
-        Job jobOne = new Job();
-        Job jobTwo = new Job();
+        jobOne = new Job();
+        jobTwo = new Job();
     }
 
+    @Test
+    public void testSettingJobId() {
+        assertFalse(jobOne.getId() == jobTwo.getId());
+    }
 }
