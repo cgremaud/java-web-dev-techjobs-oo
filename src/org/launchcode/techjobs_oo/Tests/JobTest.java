eca5a1c2067472813ae891d5b602a3b221b3a13b
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-10 19:14:52 -0500

    Wrote test to see if Job toString returns a blank before and after job info

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -2,7 +2,7 @@ package org.launchcode.techjobs_oo.Tests;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +18,33 @@ public class JobTest {
 
     @Test
     public void testSettingJobId() {
-        assertFalse(jobOne.getId() == jobTwo.getId());
+        assertNotEquals(jobOne.getId(), jobTwo.getId());
     }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job jobFull = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(jobFull.getName(), "Product tester");
+        assertEquals(jobFull.getEmployer().getValue(), "ACME");
+        assertEquals(jobFull.getLocation().getValue(), "Desert");
+        assertEquals(jobFull.getPositionType().getValue(), "Quality control");
+        assertEquals(jobFull.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job jobA = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job jobB = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        //don't use assertNotEquals here because you actually want to test the custom equals() method.
+        assertFalse(jobA.equals(jobB));
+    }
+
+    @Test
+    public void testToStringReturnsBlankBeforeAndAfterJobInfo() {
+        Job jobA = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(" ", jobA.toString().charAt(0));
+        assertEquals(" ", jobA.toString().charAt(jobA.toString().length() - 1));
+
+    }
+
 }
