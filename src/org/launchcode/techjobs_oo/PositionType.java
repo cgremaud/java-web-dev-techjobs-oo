23f0fe45e66a62585fb41cf6d22ab13b462c14f3
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-10 22:04:40 -0500

    deleted unnecessary, commented out code

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -4,48 +4,8 @@ import javax.swing.text.Position;
 import java.util.Objects;
 
 public class PositionType extends JobField{
-//    private int id;
-//    private static int nextId = 1;
-//    private String value;
-//
-//    public PositionType() {
-//        id = nextId;
-//        nextId++;
-//    }
 
     public PositionType(String value) {
         super(value);
     }
-//
-//    // TODO: Add a custom toString() method that returns the data stored in 'value'.
-//    @Override
-//    public String toString(){
-//        return value;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        PositionType that = (PositionType) o;
-//        return id == that.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-//// Getters and Setters:
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(String value) {
-//        this.value = value;
-//    }
 }
