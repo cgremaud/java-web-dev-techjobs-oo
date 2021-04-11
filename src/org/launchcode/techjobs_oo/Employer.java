e8365dd536b0a91ae64d62c2349b64b8e81b9f26
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-10 21:38:53 -0500

    Employer now extends JobField

--- src/org/launchcode/techjobs_oo/Employer.java
+++ src/org/launchcode/techjobs_oo/Employer.java
@@ -2,19 +2,18 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class Employer {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Employer() {
-        id = nextId;
-        nextId++;
-    }
+public class Employer extends JobField {
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public Employer() {
+//        id = nextId;
+//        nextId++;
+//    }
 
     public Employer(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
     // Custom toString, equals, and hashCode methods:
@@ -24,18 +23,18 @@ public class Employer {
         return value;
     }
 
-    @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
-        if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
-        Employer employer = (Employer) o;
-        return getId() == employer.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+//    @Override
+//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+//        if (this == o) return true;
+//        if (!(o instanceof Employer)) return false;
+//        Employer employer = (Employer) o;
+//        return getId() == employer.getId();
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
 
     // Getters and Setters:
 
